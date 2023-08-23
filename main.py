cddd3fc9bf2c930f5774d01f55cdc75b820f90e5
manzalana <142954577+manzalana@users.noreply.github.com>
2023-08-23 02:37:03 +0000

    Starter code
    1. I created a list containing the possible options for the user to choose from and set a variable to determine if the program is currently active.
    2. welcomed the user
    3. stored the user's name and age in a variable
    4. asked user how they need to be helped using an F string
    5. created an empty response variable, which will be how the chat bot responds
    6. prompts user for their choice
    7. using a series of elif statements, I set the response variable to what the user chose
    8. if the user chooses the end the program, it sets active to False and ends the program

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('hello world!!!')
+
+'''
+Welcome the user
+Collect the user’s name and age
+Ask the user how it can help them
+Allow the user to choose from a menu/list of options on how they can continue the conversation
+
+
+'''
+convo_choices=['This is a response to option 1', 'This is a response to option 2', 'Goodbye, I hope you have a good day!']
+active=True
+
+print('Hello User!')
+name=input('What is your name? ')
+age=input('How old are you? ')
+print(f"How can I assist you today, {name}?")
+
+
+
+response=''
+while active:
+  choice=input('This will be a menu\n1. option one\n2. option two\n3. Exit program  ')
+  if choice=='1':
+    response=convo_choices[0]
+  elif choice=='2':
+    response=convo_choices[1]
+  elif choice=='3':
+    response=convo_choices[2]
+    active=False
+  else:
+    response='Im sorry, I dont understand :('
+  print(response)
+
